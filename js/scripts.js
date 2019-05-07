e9e10b50e299c96f34c10f1e62ac5455ec97cefe
Jose Horta <joshhortt@yahoo.com>
2019-05-07 18:58:47 +0100

    add indentation'.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,21 @@
 // Business Logic
-
 var stringOne = [];
-var stringTwo =[];
+var stringTwo = [];
 var returnString = "";
 
 // User Interface Logic
-$(function(){
-  $("#btn-submit").click(function(event){
-    event.preventDefault();
-    stringOne = $("#sentence").val().split(" ");
-    stringTwo = [];
-    stringOne.forEach(function(word) {
-      if(word.length >= 3)
-      {
-        stringTwo.push(word);
-      }
-    });
-    returnString = stringTwo.reverse().join(" ");
-    $("#result").text(returnString);
+$(function () {
+    $("#btn-submit").click(function (event) {
+        event.preventDefault();
+        stringOne = $("#sentence").val().split(" ");
+        stringTwo = [];
+        stringOne.forEach(function (word) {
+            if (word.length >= 3) {
+                stringTwo.push(word);
+            }
+        });
+        returnString = stringTwo.reverse().join(" ");
+        $("#result").text(returnString);
 
-  });
+    });
 });
